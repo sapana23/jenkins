183f32ad7c7d4be0914380cc3246d7b311e8df4c
sapana23 <80168299+sapana23@users.noreply.github.com>
2021-03-14 23:08:28 +0530

    Update TC_001.java

--- E2ESeleniumJenkins/src/testcases/TC_001.java
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -8,7 +8,7 @@ public class TC_001 {
 	@Test
 	public void testcase()
 	{
-		System.setProperty("webdriver.driver.chrome", "./Driver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver=new ChromeDriver();
 		driver.get("https://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
